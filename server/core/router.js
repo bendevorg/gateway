1d172247f1f8fcfee317dd84aedbf2c8dc20f254
GuiMac <guibasconti@gmail.com>
2018-06-27 19:39:23 -0300

    Generate logs

--- server/core/router.js
+++ server/core/router.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const proxy = require('express-http-proxy');
+const timer = require('../utils/timer');
 const fs = require('fs');
 const constants = require('../utils/constants');
 
 const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 
 const routesPath = process.cwd() + '/.routes';
 let routes = [];
@@ -14,6 +17,8 @@ if (fs.existsSync(routesPath)) {
     if (file.indexOf('.json') !== -1) routes.push(require(routesPath + '/' + file)[process.env.NODE_ENV]);
   });
 }
+
+router.use(timer);
 
 routes.forEach(route => {
   router.use(`/${route.name}`, proxy(route.path, { ...route.options, ...constants.routes.options }));
--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,5 +1,30 @@
-module.exports = async (req, res) => {
-  setTimeout(() => {
-    console.log(req.data);
-  }, 5000);
+module.exports = async (res, req) => {
+  res.date = Date.now();
+
+  try {
+    res.data = JSON.stringify(JSON.parse(res.data.toString('utf8')));
+  } catch (err) {
+    //  In case the data is not a JSON we just transform it into a string
+    res.data = res.data.toString('utf8');
+  }
+  const log = {
+    req: {
+      base: req.baseUrl,
+      url: req.url,
+      path: req.path,
+      method: req.method,
+      date: req.date,
+      headers: req.headers,
+      params: req.params,
+      query: req.query,
+      body: req.body
+    },
+    res: {
+      statusCode: res.statusCode,
+      date: res.date,
+      time: res.date - req.date,
+      headers: res.headers,
+      data: res.data
+    }
+  };
 };
